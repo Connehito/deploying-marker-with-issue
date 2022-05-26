74ffd4bed0f9e650df4f1e8fa0f4a98b97bbdb85
mryhryki <mryhryki@gmail.com>
2022-05-26 15:01:35 +0900

    Add example workflow and show input values to debug log

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const action = core.getInput('action')
+    const issueNumber = parseInt(core.getInput('issue-number'), 10)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`DEBUG: ${JSON.stringify({action, issueNumber})}`)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
