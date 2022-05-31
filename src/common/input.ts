b9967113fa988836b2360b742211105ee27b7ddb
mryhryki <mryhryki@gmail.com>
2022-05-31 17:26:07 +0900

    Add exit-with-error input

--- src/common/input.ts
+++ src/common/input.ts
@@ -3,13 +3,20 @@ import * as core from '@actions/core'
 export interface Input {
   action: string
   issueNumber: number
+  exitWithError: boolean
 }
 
 export const getInput = (): Input => {
   const action = core.getInput('action', {required: true})
+
   const issueNumber = parseInt(
     core.getInput('issue-number', {required: true}),
     10
   )
-  return {action, issueNumber}
+
+  const exitWithError = core.getBooleanInput('exit-with-error', {
+    required: true
+  })
+
+  return {action, issueNumber, exitWithError}
 }
